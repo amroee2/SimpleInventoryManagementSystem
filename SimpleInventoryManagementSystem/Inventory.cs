4a618ada042e09068090abcc5ec218a4ebe9b775
Amro <amroqadadha@gmail.com>
2024-08-18 11:17:46 +0300

    Add Product class and Inventory Class fields and properties

---
+++ SimpleInventoryManagementSystem/Inventory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleInventoryManagementSystem
+{
+    public class Inventory
+    {
+        private static List<Product> inventory = new List<Product>();
+
+        public Inventory() { }
+
+
+    }
+}
