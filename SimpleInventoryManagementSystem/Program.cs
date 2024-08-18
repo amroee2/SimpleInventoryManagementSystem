c3f1bdf6baaec87a92a54b7cb69eeae62418100f
Amro <amroqadadha@gmail.com>
2024-08-18 11:39:17 +0300

    Add product addition funcationality and fix bugs and identation in Program menu

--- SimpleInventoryManagementSystem/Program.cs
+++ SimpleInventoryManagementSystem/Program.cs
@@ -8,17 +8,19 @@ namespace Program
         {
             Inventory inventory = new Inventory();
             Console.WriteLine("**** Welcome ****");
-
-            Console.WriteLine("1-Add a product\n 2-View all products\n 3-Edit a product\n" +
-                " 4-Delete a product\n5-Search for a product\n 0-Close the program");
-
-            int operation = Convert.ToInt32(Console.ReadLine());
             while (true)
             {
+                Console.WriteLine("1-Add a product\n2-View all products\n3-Edit a product\n" +
+                    "4-Delete a product\n5-Search for a product\n0-Close the program");
+                int operation = Convert.ToInt32(Console.ReadLine());
+
                 switch (operation)
                 {
                     case 0:
                         Console.WriteLine("Closing Program\n");
+                        return;
+                    case 1:
+                        inventory.AddProduct();
                         break;
                 }
             }
